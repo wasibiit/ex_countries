4d72af8b35df5aa4d7ddca83b7d4f87f87fe4aa5
Wasi R <wasibiit@gmail.com>
2023-12-02 17:30:13 +0500

    Published 0.0.2 to hex

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCountries.Mixfile do
   def project do
     [
       app: :ex_countries,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
